461fea58af1ae7fb66249a74c80c04c4559efeb1
Serhii Shu <shuturmynskyi.s@gmail.com>
2023-05-13 13:13:17 +0300

    New changelist

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! This is my first serious time");
         System.out.println("And another one");
+        System.out.println("Right now I add the new feature branch");
     }
 }
